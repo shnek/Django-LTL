01b90378c2d48ef726db4693013a8109f231a3b4
Shnek <shnek94@gmail.com>
2021-12-01 09:31:12 -0500

    Refactoring to generic views

--- learning/polls/views.py
+++ learning/polls/views.py
@@ -3,6 +3,7 @@ from django.http.response import HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
@@ -48,3 +49,19 @@ def vote(request, question_id):
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
     # return HttpResponse("You're voting on question %s." % question_id)
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls.results.html'
