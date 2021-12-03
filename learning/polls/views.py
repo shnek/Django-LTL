b077da2c0286e21dbd07b44772a8f40369d83a1f
Shnek <shnek94@gmail.com>
2021-12-03 04:30:59 -0500

    Adding view tests

--- learning/polls/views.py
+++ learning/polls/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -56,7 +57,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
