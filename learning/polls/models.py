4910ed84c72dd7997c0695a2dfc0dc6fae7c286a
Shnek <shnek94@gmail.com>
2021-12-03 07:14:45 -0500

    Customizing admin panel

--- learning/polls/models.py
+++ learning/polls/models.py
@@ -1,4 +1,7 @@
 import datetime
+from django.contrib.admin import decorators
+
+from django.contrib import admin
 
 from django.db import models
 from django.utils import timezone
@@ -6,12 +9,18 @@ from django.utils import timezone
 # Create your models here.
 
 class Question(models.Model):
+    
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self) -> str:
         return self.question_text
-
+        
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self) -> bool:
         return  timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= timezone.now()
 
