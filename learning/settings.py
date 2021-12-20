ea8ff81f3cceeef2445072e7a44f81e6bbae96b2
Jakub Synowiec <shnek@jakubs-macbook-pro.home>
2021-12-20 07:07:00 -0500

    Fixing basedir

--- learning/settings.py
+++ learning/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
