398183c82a982db05c25f358959f5cee4334de0e
Jakub Synowiec <shnek@jakubs-macbook-pro.home>
2021-12-20 06:46:34 -0500

    Fixing wsgi and asgi

--- learning/wsgi.py
+++ learning/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning.learning.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning.settings')
 
 application = get_wsgi_application()
