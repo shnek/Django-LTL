a37eb624092f3b31a1c1cf0b2d8d2c4ad98f351a
Jakub Synowiec <shnek@jakubs-macbook-pro.home>
2021-12-20 05:32:35 -0500

    Adding auth to a view

--- learning/polls/views.py
+++ learning/polls/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Choice, Question
 
@@ -38,17 +39,21 @@ def results(request, question_id):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    try: 
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice."
-        })
+
+    if request.user.is_authenticated:
+        try: 
+            selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        except (KeyError, Choice.DoesNotExist):
+            return render(request, 'polls/detail.html', {
+                'question': question,
+                'error_message': "You didn't select a choice."
+            })
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
     else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponse("You have to be logged in to vote on a question!")
     # return HttpResponse("You're voting on question %s." % question_id)
 
 class IndexView(generic.ListView):
@@ -59,7 +64,8 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
-class DetailView(generic.DetailView):
+class DetailView(LoginRequiredMixin, generic.DetailView):
+    login_url = "/admin/login/"
     model = Question
     template_name = 'polls/detail.html'
 
