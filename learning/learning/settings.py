7cc9213ec2acaefcd46830326d9c87e8b8e26ffb
Jakub Synowiec <shnek@jakubs-macbook-pro.home>
2021-12-20 06:02:09 -0500

    Adding deployment config

--- learning/learning/settings.py
+++ learning/learning/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2(cu$lksg!g$tm+3v0am6!z-f3y8%^i*3l&2pd774ucvwyw%gi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://django-ltl.herokuapp.com/',
+    '127.0.0.1'
+    ]
 
 
 # Application definition
@@ -124,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
